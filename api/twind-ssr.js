4e34868bc3462959e48f21988310e95620a9ae66
La Ode Muhammad Al Fatih <lamualfa@gmail.com>
2020-12-26 18:43:36 +0700

    Fix Twind SSR error.

--- api/twind-ssr.js
+++ api/twind-ssr.js
@@ -1,6 +1,7 @@
 const { setup } = require('twind');
 const { virtualSheet, getStyleTag } = require('twind/server');
 const tailwindConfig = require('../tailwind.config');
+const { tossr } = require('tossr');
 
 const sheet = virtualSheet();
 setup({ ...tailwindConfig, sheet });
@@ -8,7 +9,5 @@ setup({ ...tailwindConfig, sheet });
 module.exports = async function twindSsr(template, script, url, options) {
   sheet.reset();
   const html = await tossr(template, script, url, options);
-  html.replace('</head>', `${getStyleTag(sheet)}</head>`);
-
-  return html;
+  return html.replace('</head>', `${getStyleTag(sheet)}</head>`);
 };
