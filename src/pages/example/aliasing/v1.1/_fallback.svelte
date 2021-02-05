f7aa677916130da21396a13419ec83b07003217f
La Ode Muhammad Al Fatih <lamualfa@gmail.com>
2021-02-06 06:36:18 +0700

    Fix conflict after merging

--- src/pages/example/aliasing/v1.1/_fallback.svelte
+++
@@ -1,4 +0,0 @@
-<script>
-    import {goto, leftover} from '@roxi/routify'
-    $goto('../../v1/'+$leftover, null, true, true)
-</script>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,27 +1,17 @@
 <script>
-<<<<<<< HEAD
     import { tw } from "twind";
 </script>
 
-<div class={tw`p-4 m-4 border border-purple-600 rounded bg-gray-100`}>
+<div class={tw`p-4 m-4 border(& purple-600) rounded bg-gray-100`}>
     <h2
-        class={tw`text-3xl font-medium text-primary cursor-pointer hover:(underline)`}>
+        class={tw`text(3xl primary) font-medium cursor-pointer hover:(underline)`}>
         Routify + Twind (Tailwind CSS in JS version) template
     </h2>
     <p class={tw`mt-3`}>
         Visit
         <a
-            class={tw`text-blue-600 hover:text-blue-800`}
+            class={tw`text(blue-600 hover:blue-800)`}
             href="https://github.com/lamualfa/routify-twind">Official Repository</a>
         for further information.
     </p>
 </div>
-=======
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
-    import { metatags } from '@roxi/routify'
-    metatags.title = 'My Routify app'
-    metatags.description = 'Description coming soon...'
-</script>
-
-<RoutifyIntro />
->>>>>>> 6dbdc96a68c338533ad96802c64d64d47fa990b0
